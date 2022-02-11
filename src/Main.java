d8b25eebaec60f5cf713593f36dbaa25732d9d16
Devan Ng <dng8597@bths.edu>
2022-02-11 17:54:17 -0500

    removed a comment

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ public class Main
 {
     public static void main(String[] args)
     {
-        //DEVAN NG - U5T4-5 Lab: Accessor & Mutator Methods
 
 
         DragonSlayer game = new DragonSlayer();
